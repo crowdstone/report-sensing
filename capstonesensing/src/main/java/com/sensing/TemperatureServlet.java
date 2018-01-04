8e1ecd0915df272923b001992ae9072cbe4c7acf
Asteip <abonnin44@gmail.com>
2018-01-04 14:27:28 +0100

    Initialize project

---
+++ capstonesensing/src/main/java/com/sensing/TemperatureServlet.java
@@ -0,0 +1,28 @@
+package com.sensing;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "Temperature",
+    urlPatterns = {"/temperature"}
+)
+public class TemperatureServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+      
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+    
+    // TODO : get result of temperature sensing
+
+  }
+}
