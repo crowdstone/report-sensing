e53d386f4c38eda36b8e94c6e185060e0eca17a5
Asteip <abonnin44@gmail.com>
2018-01-05 20:48:19 +0100

    ajout verif sur les coordonnées

--- capstonesensing/src/main/java/com/sensing/api/TemperatureEndpoint.java
+++ capstonesensing/src/main/java/com/sensing/api/TemperatureEndpoint.java
@@ -8,6 +8,7 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.BadRequestException;
 import com.googlecode.objectify.ObjectifyService;
 import com.sensing.entity.TemperatureEntity;
 
@@ -17,19 +18,28 @@ public class TemperatureEndpoint {
 	static {
 		ObjectifyService.register(TemperatureEntity.class);
 	}
-	
+
+	/*
+	 * Check if latitude and longitude values are correct
+	 */
+	private boolean checkCoords(int latInf, int latSup, int longInf, int longSup) {
+		boolean checkLat = latInf >= -90 && latInf <= 90 && latSup >= -90 && latInf <= 90;
+		boolean checkLong = longInf >= -180 && longInf <= 180 && longSup >= -180 && longSup <= 180;
+		return checkLat && checkLong;
+	}
+
 	/**
 	 * This method gets all entities. It uses HTTP GET method.
 	 *
 	 * @return The list of entities.
 	 */
 	@ApiMethod(name = "listTemperatureEntity", httpMethod = HttpMethod.GET, path = "temperature")
-	public List<TemperatureEntity> listTemperatureEntity(){
+	public List<TemperatureEntity> listTemperatureEntity() {
 		List<TemperatureEntity> res = ofy().load().type(TemperatureEntity.class).list();
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * This method gets the entity having primary key id. It uses HTTP GET method.
 	 *
@@ -51,12 +61,18 @@ public class TemperatureEndpoint {
 	 * @param TemperatureEntity
 	 *            the entity to be inserted.
 	 * @return The inserted entity.
+	 * @throws BadRequestException If arguments are not correct
 	 */
 	@ApiMethod(name = "insertTemperatureEntity", httpMethod = HttpMethod.POST, path = "temperature")
-	public TemperatureEntity insertTemperatureEntity(TemperatureEntity temperatureEntity) {
+	public TemperatureEntity insertTemperatureEntity(TemperatureEntity temperatureEntity) throws BadRequestException {
+		if (!checkCoords(temperatureEntity.getLatInf(), temperatureEntity.getLatSup(), temperatureEntity.getLongInf(),
+				temperatureEntity.getLongSup())) {
+			throw new BadRequestException("Geographic coordinate must be in existing range (lat : [-90;90], long : [-180;180])");
+		}
+
 		temperatureEntity.updateId();
 		ofy().save().entity(temperatureEntity).now();
-		
+
 		return temperatureEntity;
 	}
 
@@ -67,12 +83,18 @@ public class TemperatureEndpoint {
 	 * @param TemperatureEntity
 	 *            the entity to be updated.
 	 * @return The updated entity.
+	 * @throws BadRequestException If arguments are not correct
 	 */
 	@ApiMethod(name = "updateTemperatureEntity", httpMethod = HttpMethod.PUT, path = "temperature")
-	public TemperatureEntity updateTemperatureEntity(TemperatureEntity temperatureEntity) {
+	public TemperatureEntity updateTemperatureEntity(TemperatureEntity temperatureEntity) throws BadRequestException {
+		if (!checkCoords(temperatureEntity.getLatInf(), temperatureEntity.getLatSup(), temperatureEntity.getLongInf(),
+				temperatureEntity.getLongSup())) {
+			throw new BadRequestException("Geographic coordinate must be in existing range (lat : [-90;90], long : [-180;180])");
+		}
+		
 		temperatureEntity.updateId();
 		ofy().save().entity(temperatureEntity).now();
-		
+
 		return temperatureEntity;
 	}
 
